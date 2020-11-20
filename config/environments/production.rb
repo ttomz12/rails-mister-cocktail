a866f4915f85db45b3a527dd2c2039750b1b4cd5
Taro Tomiya <taro.tomiya@gmail.com>
2020-11-20 10:31:49 -0500

    updated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,9 +38,9 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  #config.assets.debug = true
-  #config.assets.check_precompiled_asset = false
-  #config.serve_static_files = true
+  config.assets.debug = true
+  config.assets.check_precompiled_asset = false
+  config.serve_static_files = true
 
   #test
 
