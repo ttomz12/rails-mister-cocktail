e7c7ea344e6b530601c14f6fd04ce530c963de96
Taro Tomiya <taro.tomiya@gmail.com>
2020-11-20 12:55:54 -0500

    added images

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
+
 puts "Destroying all the data..."
 
 Cocktail.destroy_all
@@ -12,17 +14,15 @@ Ingredient.destroy_all
 
 puts "Populating database with cocktails and ingredients"
 
+file = URI.open('https://images.kitchenstories.io/recipeImages/LongIslandIcedTea_4x3/LongIslandIcedTea_4x3-large-landscape-150.jpg')
+cocktail = Cocktail.new(name: 'Long Island Iced Tea')
+cocktail.photo.attach(io: file, filename: 'liit.jpg', content_type: 'image/jpg')
+
+Cocktail.create(name: "Old fashioned")
+
 Ingredient.create(name: "Lemon")
 Ingredient.create(name: "Ice")
 Ingredient.create(name: "Mint leaves")
 Ingredient.create(name: "Olives")
-Ingredient.create(name: "Vodka")
-
-Cocktail.create(name: "Old fashioned")
-Cocktail.create(name: "Manhattan")
-Cocktail.create(name: "Negroni")
-Cocktail.create(name: "Aperol Spritz")
-
-Dose.create(description: "Very bitter", ingredient_id: 4, cocktail_id: 10)
 
 puts "Done"
